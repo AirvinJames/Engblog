089dfa9f134148ac0940462419767766b01698ab
Airvin James L. Medina <airvinjames0216@gmail.com>
2023-11-29 08:42:13 +0800

    finished homepage for real

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
-let clouds = document.getElementsByClassName('cloud');
-
+const clouds = document.getElementsByClassName('cloud');
+// const mouse = document.querySelector('#blob');
 
 for (var i = 0; i < clouds.length; i++) {
   let item = clouds[i];
 
   let randomNumber = (Math.random() * (3 - 1) + 1).toFixed(2);
   item.style['animation-duration'] = randomNumber.toString() + 's';
+}
+
+window.onmousemove = event => {
+  const dots = document.getElementsByClassName('dots')
+  for (let i = 0; i < dots.length; i++) {
+    let dot = dots[i];
+
+    const { x, y } = event
+    dot.style.translate = `${x / 90}px ${y / 90}px`
+  }
 }
