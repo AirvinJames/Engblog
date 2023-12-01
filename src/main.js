f9690bf26c38b402a110a6805ab0970a7b5188b9
Airvin James L. Medina <airvinjames0216@gmail.com>
2023-12-01 11:26:49 +0800

    done

--- src/main.js
+++ src/main.js
@@ -7,3 +7,9 @@ for (var i = 0; i < clouds.length; i++) {
   let randomNumber = (Math.random() * (3 - 1) + 1).toFixed(2);
   item.style['animation-duration'] = randomNumber.toString() + 's';
 }
+
+/*function bomb() {
+  document.body.style.opacity = 0;
+  document.body.style.background = 'none';
+}
+*/
